2f726f806a0e1ac485d5cf37d3c082f959aa65dc
IonaCS <ionakat@gmail.com>
2015-08-27 11:25:37 +1200

    attempt scroll with plugin

--- other_project/js/overflow-text.js
+++ other_project/js/overflow-text.js
@@ -1,24 +1,3 @@
-$(document).ready(function () {
-  $('.songs li').mouseenter(function () {
-    $(this).stop();
-    var boxWidth = $(this).width();
-    var textWidth = $('p', $(this)).width();
-    if (textWidth > boxWidth) {
-      var animSpeed = textWidth * 10;
-      $(this).animate({
-        scrollLeft: (textWidth - boxWidth)
-      }, animSpeed, function () {
-        $(this).animate({
-          scrollLeft: 0
-        }, animSpeed, function () {
-          $(this).trigger('mouseenter');
-        });
-      });
-    }
-  }).mouseleave(function () {
-    var animSpeed = $(this).scrollLeft() * 10;
-    $(this).stop().animate({
-      scrollLeft: 0
-    }, animSpeed);
-  });
+$('.marquee').marquee({
+  duplicated: true
 });
