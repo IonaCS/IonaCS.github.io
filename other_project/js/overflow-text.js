cd735cd68328f161e0e047a90abe0efae4bf7b6d
IonaCS <ionakat@gmail.com>
2015-08-27 13:46:08 +1200

    more attempts

--- other_project/js/overflow-text.js
+++ other_project/js/overflow-text.js
@@ -1,7 +1,12 @@
-$( ".songs li p" ).hover(
-  function() {
-    $( this ).marquee( { duplicated: true } );
-  }, function() {
-    $( this ).marquee( "pause" );
-  }
-);
+// $( ".songs li p" ).hover(
+//   function() {
+//     $( this ).marquee( { duplicated: true } );
+//   }, function() {
+//     $( this ).marquee( "pause" );
+//   }
+// );
+
+
+$('.song li p').hover(function(){
+  $(this).marquee('toggle');
+});
