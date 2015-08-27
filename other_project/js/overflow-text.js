1e561151820ee5bcc6038bd921543749d89392e6
name": null <email": null>
2015-08-27 16:50:20 +1200

    it's workinggit add .git add .

--- other_project/js/overflow-text.js
+++ other_project/js/overflow-text.js
@@ -1,3 +1,14 @@
-$('.song li p').marquee({
-  duplicated: true
+$('.songs li p').on('mouseover', function() {
+  if ($(this).children().length > 0) {
+    $(this).marquee("resume")
+  } else {
+    $(this).marquee();
+  }
 });
+
+$("p").hover(function(){
+  $(this).marquee('resume');
+}, function(){
+  console.log(this)
+  $(this).marquee('pause');
+})
